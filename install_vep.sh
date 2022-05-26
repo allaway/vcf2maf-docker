9d7a5d6c93e25599aa8913dea7dbf8f225d463ec
Robert Allaway <allaway@users.noreply.github.com>
2022-05-26 16:26:44 -0700

    Update install_vep.sh

--- install_vep.sh
+++ install_vep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vep_install --AUTO cf --SPECIES homo_sapiens --ASSEMBLY GRCh38 --CACHEDIR $HOME/.vep
+vep_install --AUTO cf --SPECIES homo_sapiens --ASSEMBLY GRCh38 --CACHEDIR $HOME/.vep --NO_UPDATE
